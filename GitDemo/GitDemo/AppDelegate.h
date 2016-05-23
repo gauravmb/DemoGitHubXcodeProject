cb25da2da6e77ba1b11658d25d523f2ed73f4a20
Gaurav Bhatia <gaurav.bhatia@hurix.com>
2016-05-23 16:59:47 +0530

    Created Project

---
+++ GitDemo/GitDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  GitDemo
+//
+//  Created by Gaurav Bhatia on 5/23/16.
+//  Copyright © 2016 Gaurav Bhatia. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ GitDemo/GitDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  GitDemo
+//
+//  Created by Gaurav Bhatia on 5/23/16.
+//  Copyright © 2016 Gaurav Bhatia. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
